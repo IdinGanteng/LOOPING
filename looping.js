e4680ff03eafc323a6eb6be71a453bbab61af48d
riskimaulidinakbar <kanggayus101@gmail.com>
2022-07-09 14:21:02 +0700

    latihan looping

---
+++ looping.js
@@ -0,0 +1,9 @@
+var jumlahBECAK=10
+var beroperasi=6
+var nomor=1 
+while(nomor<=beroperasi){console.log('becak no.'+nomor+++' beroperasi')} 
+
+    for(nomor=7;nomor<=jumlahBECAK;nomor++){
+        console.log('becak no.'+nomor+' tidak beroperasi')
+    }
+
